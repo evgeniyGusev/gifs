55f586084e909ebe3946a41785a59c5a3b652fc4
Evgeniy <shchelkalinevgeniy@gmail.com>
2021-02-11 08:04:03 +0300

    'header-styles'

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -20,9 +20,17 @@
             {{ link.title }}
           </nuxt-link>
         </li>
+        <li class="nav-item">
+          <div class="nav-link">
+            <span class="_dots">...</span>
+          </div>
+        </li>
       </ul>
     </nav>
-    <div class="control"></div>
+    <div class="control">
+      <button class="control-btn">Upload</button>
+      <button class="control-btn">Create</button>
+    </div>
     <div class="user"></div>
   </header>
 </template>
@@ -64,17 +72,54 @@ header {
       display: flex;
       justify-content: flex-start;
       align-items: center;
-      padding: 0 0 5px;
+      padding: 0 0 4px;
       background: linear-gradient(to right, rgb(0, 204, 255), rgb(153, 51, 255) 31%, rgb(230, 70, 182) 52%, rgb(255, 249, 170) 77%, rgb(0, 255, 153), rgb(0, 204, 255)) 0% 50% / 200% 50%;
 
       .nav-item {
-        background-color: $mainBG;
+        background-color: rgba(18, 18, 18, 1);
+        transition: background-color .3s;
+
+        &:hover {
+          background-color: rgba(18, 18, 18, 0);
+        }
 
         .nav-link {
+          position: relative;
+          display: block;
+          padding: 8px 14px 4px 10px;
           font-size: 15px;
           font-weight: 600;
+          color: #fff;
+          cursor: pointer;
+          
+          &::after {
+            content: '';
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 4px;
+            height: calc(100% + 4px);
+            background-color: rgb(18, 18, 18);
+          }
+        }
+
+        ._dots {
+          display: flex;
+          position: relative;
+          left: 3px;
+          letter-spacing: 1px;
+          font-weight: 800;
+          transform: rotate(90deg);
         }
       }
+    }
+  }
+
+  .control {
+    .control-btn {
+      @include button-default;
+      padding: 8.5px 16px;
+      background-color: #6157ff;
     }
   }
 }
